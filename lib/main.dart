3938e48423edec22b48aad168d221338ca2a587d
David Atlan <david@davipro.fr>
2023-08-06 13:24:09 +0200

    Multiple animated icons was created

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,31 @@ class MyApp extends StatelessWidget {
           title: const Text('Animation Dynamique'),
         ),
         body: const Center(
-          child: IconLogo(iconName: Icons.directions_bike, iconColor: Colors.blue),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconLogo(
+                iconName: Icons.directions_walk,
+                iconColor: Colors.green,
+                animationDuration: Duration(milliseconds: 500)),
+              IconLogo(
+                  iconName: Icons.directions_run,
+                  iconColor: Colors.lightBlue,
+                  animationDuration: Duration(milliseconds: 600)),
+              IconLogo(
+                  iconName: Icons.directions_bike,
+                  iconColor: Colors.purple,
+                  animationDuration: Duration(milliseconds: 700)),
+              IconLogo(
+                  iconName: Icons.favorite,
+                  iconColor: Colors.pink,
+                  animationDuration: Duration(milliseconds: 800)),
+              IconLogo(
+                  iconName: Icons.more_vert,
+                  iconColor: Colors.grey,
+                  animationDuration: Duration(milliseconds: 900)),
+            ],
+          ),
         ),
       ),
     );
@@ -28,16 +52,21 @@ class MyApp extends StatelessWidget {
 class IconLogo extends StatelessWidget {
   final IconData iconName;
   final Color iconColor;
+  final Duration animationDuration;
 
-  const IconLogo({super.key, required this.iconName, required this.iconColor});
+  const IconLogo(
+      {super.key,
+      required this.iconName,
+      required this.iconColor,
+      required this.animationDuration});
 
   @override
   Widget build(BuildContext context) {
     return Animator<double>(
       tween: Tween<double>(begin: 0, end: 50),
       cycles: 1,
-      duration: const Duration(seconds: 1),
-      builder: (context, animatorState, child ) => Container(
+      duration: animationDuration,
+      builder: (context, animatorState, child) => Container(
         width: animatorState.value,
         height: animatorState.value,
         decoration: BoxDecoration(
@@ -48,14 +77,14 @@ class IconLogo extends StatelessWidget {
               color: Colors.white.withOpacity(0.1),
               spreadRadius: 3,
               blurRadius: 15,
-              offset: Offset(0, 3),
+              offset: const Offset(0, 3),
             ),
           ],
         ),
         child: Icon(
           iconName,
           color: Colors.white,
-          size: animatorState.value /2,
+          size: animatorState.value / 2,
         ),
       ),
     );
